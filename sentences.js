4bdc6590735ab76a416b5fcbb4259d9f177a24f0
ghoul <148587390+ghoullic@users.noreply.github.com>
2024-01-18 19:02:09 +0000

    Update and rename sentences.js to sentences.txt

--- sentences.js
+++
@@ -1,9 +0,0 @@
-// sentences.js
-
-const sentences = [
-    "This is a random sentence.",
-    "Another random sentence here.",
-    "Generate more sentences for your game.",
-    "Customize as needed.",
-    // Add more sentences as desired
-];
